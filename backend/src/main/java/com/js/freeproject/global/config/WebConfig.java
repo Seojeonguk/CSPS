e750047c8d03cc2b8d37c27f4d2382bf275f1905
Seojeonguk <uk7880@naver.com>
2021-11-04 15:07:05 +0900

    [BE] refactor : Move config file

--- backend/src/main/java/com/js/freeproject/global/config/WebConfig.java
+++ backend/src/main/java/com/js/freeproject/global/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.js.freeproject.global.jwt;
+package com.js.freeproject.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
