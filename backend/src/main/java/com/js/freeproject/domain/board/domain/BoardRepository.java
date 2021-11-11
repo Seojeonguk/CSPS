08c385fb4016a6f57ca21804bfcc04d8268b4395
KingBlackCow <sgs1159@naver.com>
2021-11-11 11:10:14 +0900

    [BE] fix : board 최신순으로 가져오도록 변경

--- backend/src/main/java/com/js/freeproject/domain/board/domain/BoardRepository.java
+++ backend/src/main/java/com/js/freeproject/domain/board/domain/BoardRepository.java
@@ -13,7 +13,9 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     @EntityGraph(attributePaths = {"comments"})
     Optional<Board> findById(Long id);
 
+    @Override
     @EntityGraph(attributePaths = {"user"})
+    @Query("select b from Board b order by b.createDate desc")
     List<Board> findAll();
 
     @Query("select b from Board b where b.user = :user")
