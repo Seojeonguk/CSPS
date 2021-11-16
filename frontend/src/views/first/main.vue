7ef8aa2b506e86026b97dfa4caf3e2a0a7b86310
KingBlackCow <sgs1159@naver.com>
2021-11-16 11:50:46 +0900

    [FE] fix : 버튼 애니매이션

--- frontend/src/views/first/main.vue
+++ frontend/src/views/first/main.vue
@@ -2,7 +2,7 @@
   <div class="first-wrap">
     <div class="first-left"></div>
     <div class="first-right">
-      <span class="first-title">CS의 정석</span>
+      <span class="first-title" data-aos="fade-up">CS의 정석</span>
       <div class="first-buttons">
         <div class="login-btn" @click="showLogin">로그인</div>
         <div class="regist-btn" @click="showRegist">회원가입</div>
